297aea202eb5b142cf257230ed478f62f45c7b2b
SamMalcolm <sam_malcolm@live.com.au>
2020-07-20 16:20:58 +1000

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	console.log(req.method);
+	console.log(req.body);
+	res.render('index', { title: 'Express' });
+});
+router.post('/', function (req, res, next) {
+	console.log(req.method);
+	console.log(req.body);
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
