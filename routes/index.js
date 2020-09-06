c297c755465203f6444df3fde12806dda6dbe444
SamMalcolm <sam_malcolm@live.com.au>
2020-09-07 09:49:17 +1000

    added logs to start experimenting with LTI 1.3

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('*', function (req, res, next) {
 	console.log(req.method);
+	console.log(req.path);
+	console.log("body")
 	console.log(req.body);
+	console.log("params")
+	console.log(req.params);
 	res.render('index', { title: 'Express' });
 });
-router.post('/', function (req, res, next) {
+
+
+router.post('*', function (req, res, next) {
 	console.log(req.method);
+	console.log(req.path);
+	console.log("body")
 	console.log(req.body);
+	console.log("params")
+	console.log(req.params);
 	res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
