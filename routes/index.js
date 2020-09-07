baa44c7f631ec2f18a1369abf42e72c5613f82d0
SamMalcolm <sam_malcolm@live.com.au>
2020-09-07 10:54:57 +1000

    Further LTI 1.3 work

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.post("/openid", (req, res) => {
+	let payload = {};
+	payload.redirect_uri = "https://sandbox-node-js.herokuapp.com/redirect";
+	payload.client_id = "10000000000004";
+	payload.login_hint = req.body.login_hint;
+	payload.state = "nr147";
+
+	var options = {
+		'method': 'POST',
+		'url': 'https://localhost:3000/api/lti/authorize_redirect',
+		formData: payload
+	};
+	request(options, function (error, response) {
+		if (error) throw new Error(error);
+		console.log(response.body);
+	});
+
+	res.render('index', { title: req.path });
+
+
+})
+
 router.get('*', function (req, res, next) {
 	console.log(req.method);
 	console.log(req.path);
@@ -9,7 +32,7 @@ router.get('*', function (req, res, next) {
 	console.log(req.body);
 	console.log("params")
 	console.log(req.params);
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: req.path });
 });
 
 
@@ -20,7 +43,7 @@ router.post('*', function (req, res, next) {
 	console.log(req.body);
 	console.log("params")
 	console.log(req.params);
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: req.path });
 });
 
 
