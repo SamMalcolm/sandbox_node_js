52ae953ca962b530bf7ce00607d87e94a1a57649
SamMalcolm <sam_malcolm@live.com.au>
2020-09-07 15:37:57 +1000

    added oauth flow

--- routes/index.js
+++ routes/index.js
@@ -31,6 +31,12 @@ router.post("/openid", (req, res) => {
 
 })
 
+router.post("/redirect", (req, res) => {
+
+	res.redirect('https://rmit-lab.instructure.com/login/oauth2/auth?client_id=115150000000000151&response_type=code&state=YYY&redirect_uri=https://sandbox-node-js.herokuapp.com/oauth')
+
+})
+
 router.get('*', function (req, res, next) {
 	console.log(req.method);
 	console.log(req.path);
