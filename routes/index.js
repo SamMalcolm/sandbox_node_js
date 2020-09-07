d33b2ef0ec099bc14524cda7737cd843901539ae
SamMalcolm <sam_malcolm@live.com.au>
2020-09-07 15:51:19 +1000

    added oauth flow

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ router.post("/openid", (req, res) => {
 
 router.post("/redirect", (req, res) => {
 
-	res.redirect('https://rmit-lab.instructure.com/login/oauth2/auth?client_id=115150000000000151&response_type=code&state=YYY&redirect_uri=https://sandbox-node-js.herokuapp.com/oauth')
+	res.redirect('https://rmit-lab.instructure.com/login/oauth2/auth?client_id=115150000000000152&response_type=code&state=YYY&redirect_uri=https://sandbox-node-js.herokuapp.com/oauth')
 
 })
 
